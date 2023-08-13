23e8c5d2dedfe2ee383693a8065e0f3133b584e7
linh <linh.ha.nguyen20@gmail.com>
2023-08-13 23:00:07 +0700

    create new convo by button

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -7,4 +7,20 @@ class ConversationsController < ApplicationController
     @conversation = Conversation.find(params[:id])
     @messages = @conversation.messages
   end
+
+  def def new
+    @conversation = Conversation.new
+  end
+  
+  def create
+    @conversation = Conversation.new(params[:conversation])
+    if @conversation.save
+      flash[:success] = "Conversation successfully created"
+      redirect_to @conversation
+    else
+      flash[:error] = "Something went wrong"
+      render 'new', status: :unprocessable_entity
+    end
+  end
+  
 end
